80b26959844941ab46a3b60d203d4ab286a97efb
Blizzcane <blizzcane@gmail.com>
2023-03-01 20:00:09 -0600

    testing repo

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Layout from "./layout/Layout";
- 
+
+/**
+ * Defines the root application component.
+ * @returns {JSX.Element}
+ */
 function App() {
   return (
     <Switch>
